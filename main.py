1c8ab2ca53c2b4e84d26ee49e2d42bab0217ccf9
RAFazarikha <113221795+RAFazarikha@users.noreply.github.com>
2022-09-10 09:30:28 +0700

    commit pertama

---
+++ main.py
@@ -0,0 +1,30 @@
+nama = "nama saya Rakha"
+angka = 12
+angka_pecahan = 1.5
+tipe_boolean = False
+tipe_list = ["budi", "tono", "toni", 12]
+tipe_set = {1, 2, 3, 3, 3, 4, 4, "abc", "abc"}
+pertambahan = 100+5+20
+pengurangan = 100-25
+a = "nye"*5
+b = 10/2
+c = 10//3
+d = 10 % 3
+
+print(nama)
+print("tipe int", angka)
+print("tipe float", angka_pecahan)
+print("boolean : ")
+print(tipe_boolean)
+print(tipe_list)
+print(tipe_list[0])
+print(tipe_list[1])
+print(tipe_list[:2])
+print(tipe_set)
+print(5+5)
+print(pertambahan)
+print(pengurangan)
+print(a)
+print(b)
+print(c)
+print("modulus dari 10 % 3 =", d)
