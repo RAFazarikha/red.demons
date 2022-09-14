7116641d3ee715a0103dedd4134a964bf8a96322
RAFazarikha <113221795+RAFazarikha@users.noreply.github.com>
2022-09-14 23:33:30 +0700

    tugas pertama
    tugas pelatihan coding himasi
    membuat kalkulator untuk menghitung volume balok dan kubus

---
+++ PR.py
@@ -0,0 +1,23 @@
+# Nama Rachmad Aziz Fazarikha
+#NIM : 220441100080
+#lab. MM
+
+# balok
+print("menghitung volume balok")
+
+p = int(input("masukkan panjang balok  ="))
+l = int(input("masukkan luas balok     ="))
+t = int(input("masukkan tinggi balok   ="))
+
+volumebalok = p*l*t
+
+print("volume baloknya adalah", volumebalok)
+
+# kubus
+print("menghitung volume kubus")
+
+s = int(input("masukkan panjang rusuk kubus ="))
+
+volumekubus = s*s*s
+
+print("luas kubusnya adalah", volumekubus)
